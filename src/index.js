fe4e9e7e3eb22ec536b70c918765c0e17a599414
itsreimau <private.iblmln@gmail.com>
2026-01-28 06:09:46 +0700

    refactor: Updates message analysis library

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const Events = require("./Constant/Events.js");
 const MessageType = require("./Constant/MessageType.js");
 const Formatter = require("./Helper/Formatter.js");
 const { Consolefy } = require("@mengkodingan/consolefy");
-const { analyzeMessage } = require("safety-safe");
+const { analyzeMessage } = require("guaranteed_security");
 const didYouMean = require("didyoumean");
 const mime = require("mime-types");
 
